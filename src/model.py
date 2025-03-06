0cc56cd07bd5c89f8b68a0020e262b3d0835ce0c
ilyarekun <ilyarekun.gitignore@gmail.com>
2025-03-06 20:48:06 +0100

    this is model for debugging

--- src/model.py
+++ src/model.py
@@ -14,44 +14,56 @@ class BrainCNN(nn.Module):
         super(BrainCNN, self).__init__()
         
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=3),   # valid -> padding=0
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),   # valid -> padding=0
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(2),
+            #nn.Dropout(p=0.3),
             
-            nn.Conv2d(64, 128, kernel_size=7, stride=1, padding=3),  # same -> padding=3
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # same -> padding=3
             nn.ReLU(),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(2),
+            #nn.Dropout(p=0.3),
             
-            nn.Conv2d(128, 128, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(2),
+            #nn.Dropout(p=0.3),
             
-            nn.Conv2d(128, 256, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(256),
             nn.MaxPool2d(2),
+            #nn.Dropout(p=0.3),
             
-            nn.Conv2d(256, 256, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(256),
             nn.MaxPool2d(2),
+            #nn.Dropout(p=0.3),
             
-            nn.Conv2d(256, 512, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(512),
+            #nn.MaxPool2d(2),
+            #nn.Dropout(p=0.3),
+            
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(512),
             nn.MaxPool2d(2)
+            #nn.Dropout(p=0.3)
         )
         self.fc_layers = nn.Sequential(
             nn.Linear(512 * 3 * 3, 1024),
             nn.ReLU(),
-            nn.Dropout(p=0.25),
+            nn.Dropout(p=0.3),
             
             nn.Linear(1024, 512),
             nn.ReLU(),
-            nn.Dropout(p=0.25),
+            nn.Dropout(p=0.3),
             
             nn.Linear(512, 4)
             #nn.Softmax(dim=1)
